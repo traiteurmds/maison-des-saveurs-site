84a335e120edab844334792bf6947bb74edc3307
traiteurmds <contact.mds.traiteur@gmail.com>
2026-03-09 12:49:31 +0100

    update site

--- app/components/GoogleMapReviews.tsx
+++ app/components/GoogleMapReviews.tsx
@@ -1,11 +1,10 @@
 "use client";
 
-import { motion } from "framer-motion";
-
 export default function GoogleMapReviews() {
   return (
-    <section id="avis" className="border-t border-deep-green/10 bg-beige py-24">
-      <div className="mx-auto max-w-[900px] px-6 lg:px-8">
+    <section id="avis" className="border-t border-deep-green/10 bg-beige py-20">
+      <div className="mx-auto max-w-[1000px] px-6 lg:px-8">
+        <script src="https://elfsightcdn.com/platform.js" defer />
         <div className="flex justify-center">
           <div className="elfsight-app-086e1472-babb-4d8b-badb-86c0c9fcf1fa" />
         </div>
